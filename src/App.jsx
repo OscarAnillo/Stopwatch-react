2f330acc22df914209107420ccaf740adb5da41a
Oscar Anillo <oscarani14@gmail.com>
2023-05-11 15:54:04 -0500

    add stopwatch logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,52 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [time, setTime] = useState(0)
+  const [isRunning, setIsRunning] = useState(false);
+
+    useEffect(() => {
+      let intervalId;
+      if(isRunning) intervalId = setInterval(() => setTime((prevState) => prevState + 1), 10)
+       return () => {
+        clearInterval(intervalId)
+       }
+  }, [time, isRunning])
+
+  // Hours
+  const hours = Math.floor(time / 360000);
+  
+  // Minutes
+  const minutes = Math.floor((time % 360000) / 6000);
+
+  // Seconds
+  const seconds = Math.floor((time % 6000) / 100);
+
+  // Milliseconds
+  const milliseconds = time % 100;
+
+
+  const clickHandlerStart = () => {
+    setIsRunning(!isRunning)
+  }
+  
+  const clickHandlerReset = () => {
+    setTime(0)
+  }
 
   return (
     <div className='App'>
       <div className='timer'>
-        <h1>00:00:00.00</h1>
-      <div>
-        <button>Start</button>
-        <button>Stop</button>
-        <button>Reset</button>
+          <span className='hour'>{hours}</span>
+          :<span className='minutes'>{minutes.toString().padStart(2, "0")}</span>
+          :<span className='seconds'>{seconds.toString().padStart(2, "0")}</span>
+          .<span className='milliseconds'>{milliseconds.toString().padStart(2, "0")}</span>
       </div>
+      <div className='buttons'>
+        <button onClick={clickHandlerStart}>
+          {isRunning ? "Stop" : "Start"}
+        </button>
+        <button onClick={clickHandlerReset}>Reset</button>
       </div>
     </div>
   )
